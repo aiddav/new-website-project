ceecc8cca5d70e2ef9fa31617a2cdf895fda4f74
Aidan <aidandavies2@gmail.com>
2024-12-31 23:42:44 +0000

    Updated Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Logos from './components/Logos'
+import WhatWeDo from './components/WhatWeDo'
+import HowWeWork from './components/HowWeWork'
+import WhoWeAre from './components/WhoWeAre'
+import Footer from './components/Footer'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-background">
+      <Header />
+      <main>
+        <Hero />
+        <Logos />
+        <WhatWeDo />
+        <HowWeWork />
+        <WhoWeAre />
+      </main>
+      <Footer />
     </div>
   )
 }
