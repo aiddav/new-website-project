c42ea5049f798bf01b0ae98403a52981ac4cff7b
Aidan <aidandavies2@gmail.com>
2024-12-31 23:34:48 +0000

    Simplified page for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Logos from './components/Logos'
-import WhatWeDo from './components/WhatWeDo'
-import HowWeWork from './components/HowWeWork'
-import WhoWeAre from './components/WhoWeAre'
-import Footer from './components/Footer'
-
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <main>
-        <Hero />
-        <Logos />
-        <WhatWeDo />
-        <HowWeWork />
-        <WhoWeAre />
-      </main>
-      <Footer />
+    <div>
+      <h1>Hello World</h1>
     </div>
   )
 }
